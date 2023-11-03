9d7e5acd25ebb5ffd4a7eb0005ab22f3fa15d6f0
Sergey Davydov <boabab477@gmail.com>
2023-11-03 16:54:00 +0300

    fix

--- bin/install.sh
+++ bin/install.sh
@@ -29,30 +29,8 @@ _ERR_="${RED}[ERR] ${RESET}"
 conf_base_path=$(realpath ../)
 echo "$conf_base_path"
 
-read -rp "Укажите каталог установки tegia-node [/tegia]: " tegia_folder
-tegia_folder="/tegia"
 
-#
-# Install Sphinx Search
-#
-
-cd $tegia_folder
-mkdir -p ./sphinx
-cd ./sphinx
-wget -N http://sphinxsearch.com/files/sphinx-3.1.1-612d99f-linux-amd64.tar.gz
-tar zxf sphinx-3.1.1-612d99f-linux-amd64.tar.gz
-
-#
-# COPY DATA
-#
-
-cp -avr $conf_base_path/actors/A2Sphinx/sphinx/sphinxdata $tegia_folder/sphinx/sphinxdata
-cp -avr $conf_base_path/actors/A2Sphinx/sphinx/configs $tegia_folder/sphinx/configs
-
-#
-
-ln -s $conf_base_path/actors/A2Sphinx/sphinx/indexing.sh $tegia_folder/sphinx/indexing.sh
-ln -s $conf_base_path/actors/A2Sphinx/sphinx/generate_conf.sh $tegia_folder/sphinx/generate_conf.sh
+tegia_folder="$TEGIA_PATH"
 
 
 # ////////////////////////////////////////////////////////////////////////////////
